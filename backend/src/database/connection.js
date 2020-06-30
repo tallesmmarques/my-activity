b1f205e008f3c961bc33a7307dd317387dd65d5b
Talles Emanuel Moreira Marques <tallesemanuel1@gmail.com>
2020-06-30 16:58:16 -0300

    Correções no dotenv

--- backend/src/database/connection.js
+++ backend/src/database/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-require('dotenv/config')
+require('dotenv').config()
 
 mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0-6ue9w.gcp.mongodb.net/Cluster0?retryWrites=true&w=majority`)
 
