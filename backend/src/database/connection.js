79db46dc51938c334adbd2118c7c7136a60c0543
Talles Emanuel Moreira Marques <tallesemanuel1@gmail.com>
2020-06-30 14:05:04 -0300

    Fixing and delete README

--- backend/src/database/connection.js
+++ backend/src/database/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 require('dotenv/config')
 
-mongoose.connect(process.env.DATA_URL)
+mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0-6ue9w.gcp.mongodb.net/Cluster0?retryWrites=true&w=majority`)
 
 mongoose.connection.on('error', console.error.bind(console, 'connection error:'))
 mongoose.connection.once('open', () => {
